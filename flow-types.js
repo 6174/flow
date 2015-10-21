5113e97232eb18ec85176a2c7f8be495aa5b8f39
chenxuejia <chenxuejia@idcos.com>
2015-10-21 19:43:45 +0800

    初始化重构过后结构

--- flow-types.js
+++ flow-types.js
@@ -122,12 +122,12 @@ var types = [{
             document.addEventListener('mousemove', that.handleMove, false);
         },
         remove: function(i, o, that) {
-            //document.removeEventListener('mousemove',that.handleMove);
-        }
-        //	step:function(i,o,that){
-        //		o.x=mouse[0];
-        //		o.y=mouse[1];
-        //	}
+                //document.removeEventListener('mousemove',that.handleMove);
+            }
+            //  step:function(i,o,that){
+            //      o.x=mouse[0];
+            //      o.y=mouse[1];
+            //  }
     }, {
         type: 'Scope',
         info: "Plots y vs. x",
@@ -148,32 +148,25 @@ var types = [{
             box.style.height = _.h + 'px';
             box.style.backgroundColor = 'rgb(255,255,251)';
             //that.widget.resize();
-
             box.style.minWidth = 20 + 'px';
-
             //we would store w and h in that.vars,
             // but there's this issue with webkit where user can't make it smaller.
             //  we might have to make our own resizable boxes :(
-
             /*
-		_.wField=document.createElement('div');
-		box.appendChild(_.wField);
-		
-		_.wField.innerHTML='2';
-		_.wField.style.top='-10px';
-		_.wField.style.position='absolute';
-		*/
-
+        _.wField=document.createElement('div');
+        box.appendChild(_.wField);
+        
+        _.wField.innerHTML='2';
+        _.wField.style.top='-10px';
+        _.wField.style.position='absolute';
+        */
             _.canv = document.createElement('canvas');
             _.ctx = _.canv.getContext('2d');
             box.appendChild(_.canv);
-
             box.style.resize = 'both';
             box.style.overflow = 'hidden';
             _.canv.style['pointer-events'] = 'none';
-
             _.newDat = false;
-
             var pw = 0,
                 ph = 0;
             box.onmousemove = function() {
@@ -183,36 +176,26 @@ var types = [{
                     that.widget.resize();
                     that.widget.upLoc();
                     that.widget.redraw();
-
                     pw = box.clientWidth;
                     ph = box.clientHeight;
-
                     _.w = pw;
                     _.h = ph;
-
                     _.canv.width = _.w;
                     _.canv.height = _.h;
                 }
             }
-
             var pDiv = box.parentNode;
-
             _.buffer = [];
             _.dMin = 100000;
             _.dMax = -100000;
-
             _.colors = ['#00A', '#A00', '#0A0'];
-
             box.ondblclick = function() {
                 _.buffer = [];
                 _.dMin = 100000;
                 _.dMax = -100000;
-
                 that.eval();
             }
-
             that.widget.resize();
-
         },
         f: function(i, o, that) {
             var _ = that._;
@@ -229,26 +212,19 @@ var types = [{
             //_.dMin=Math.min(i.val,_.dMin);
         },
         draw: function(i, o, that) {
-
             var _ = that._;
-
             var ctx = _.ctx;
             ctx.clearRect(0, 0, _.w, _.h);
-
             var ch = 1;
             for (var p in i) {
-
                 ctx.beginPath();
                 var px = 0;
                 var ii = Math.max(_.buffer.length - sps * 2 * 10 + 1, 0);
                 for (; ii < _.buffer.length; ii++) {
-
                     //if(ii<_.buffer.length-fps*2*10)
-                    //	continue;
-
+                    //  continue;
                     var x = (_.buffer[ii][0] * _.w / 2) % _.w;
                     var y = (1 - (_.buffer[ii][ch] - _.dMin) / (_.dMax - _.dMin)) * _.h;
-
                     if (x < px) {
                         ctx.moveTo(x, y);
                     } else {
@@ -261,7 +237,6 @@ var types = [{
                 ctx.stroke();
                 ch++;
             }
-
             //buffer
         }
     }, {
@@ -279,7 +254,7 @@ var types = [{
             z: 0
         },
         vars: {
-            txt: "o.x=Math.sin(3.1*Math.PI*i.a)\n	+Math.sin(7*Math.PI*i.a)/2-3;\n\no.y=Math.random()-6;\no.z=i.a % 1;"
+            txt: "o.x=Math.sin(3.1*Math.PI*i.a)\n   +Math.sin(7*Math.PI*i.a)/2-3;\n\no.y=Math.random()-6;\no.z=i.a % 1;"
         },
         init: function(i, o, that) {
             var box = that.widget.box;
@@ -290,23 +265,17 @@ var types = [{
             //box.style.width=_.w+'px';
             //box.style.height=_.h+'px';
             var inp = document.createElement('textarea');
-
             //inp.style.minWidth='100px';
             //inp.style.maxWidth='300px';
-
             inp.style.width = _.w + 'px';
             inp.style.height = _.h + 'px';
             box.style['pointer-events'] = 'auto';
-
             box.appendChild(inp);
-
             that.widget.resize();
-
-            //			box.resize=function(){
-            //				that.widget.resize();
-            //				that.widget.redraw();
-            //			}
-
+            //          box.resize=function(){
+            //              that.widget.resize();
+            //              that.widget.redraw();
+            //          }
             inp.onkeyup = function() {
                 that.vars.txt = inp.value;
                 try {
@@ -317,10 +286,8 @@ var types = [{
                     inp.style.backgroundColor = '#FDD';
                 }
             }
-
             inp.value = that.vars.txt;
             inp.onkeyup();
-
             var pw = 0,
                 ph = 0;
             inp.onmousemove = function() {
@@ -331,7 +298,6 @@ var types = [{
                     that.widget.upLoc();
                     that.widget.redraw();
                     //that.widget.redrawWires();
-
                     pw = inp.clientWidth;
                     ph = inp.clientHeight;
                 }
@@ -354,7 +320,6 @@ var types = [{
         init: function(i, o, that) {
             var inp = document.createElement('input');
             inp.type = 'range';
-
             inp.setRange = function(min, max) {
                 var tmp = this.value;
                 this.min = min;
@@ -362,13 +327,10 @@ var types = [{
                 this.step = (max - min) / 1000;
                 this.value = tmp;
             }
-
             inp.setRange(i.min, i.max);
-
-            //		inp.min=0;
-            //		inp.max=1;
-            //		inp.step=.001;
-
+            //      inp.min=0;
+            //      inp.max=1;
+            //      inp.step=.001;
             that.widget.box.appendChild(inp);
             //that.widget.box.style.top='5px';
             that.widget.resize();
@@ -382,7 +344,6 @@ var types = [{
         },
         f: function(i, o, that) {
             that.inp.setRange(i.min, i.max);
-
             that.inp.value = i.i;
             that.inp.onchange();
         }
@@ -395,13 +356,11 @@ var types = [{
             y: 0
         },
         init: function(i, o, that) {
-
             that.tilt = function tilt(or) {
-                o.x = or[0];
-                o.y = or[1];
-            }
-
-            // http://stackoverflow.com/questions/4378435/how-to-access-accelerometer-gyroscope-data-from-javascript
+                    o.x = or[0];
+                    o.y = or[1];
+                }
+                // http://stackoverflow.com/questions/4378435/how-to-access-accelerometer-gyroscope-data-from-javascript
             if (window.DeviceOrientationEvent) {
                 window.addEventListener("deviceorientation", function() {
                     that.tilt([event.beta, event.gamma]);
@@ -415,7 +374,6 @@ var types = [{
                     that.tilt([orientation.x * 50, orientation.y * 50]);
                 }, true);
             }
-
         }
     }, {
         type: 'check',
@@ -559,94 +517,92 @@ var types = [{
         }
     }
     /*,{type:'Topic',
-	title:'Topic',
-	info:"Sends data to ANY other node with the same 'Topic Name'",
-	i:{val:0},
-	o:{val:0},
-	vars:{topic:""},
-	init:function(i,o,that){
-		var inp=document.createElement('input');
-		inp.type='input';
-		inp.value=that.vars.topic;
-		inp.placeholder='Topic Name';
-		that.widget.box.appendChild(inp);
-		that.widget.resize();
-		
-		that.joinTopic=function(){
-			
-			if(that.vars.topic){
-				
-				var topic=that.vars.topic;
-				
-				socket.emit('joinTopic',topic);
-				
-				socket.on('connect',function(){
-					socket.emit('joinTopic',topic); // re-join the topic.
-				});	
-				
-				socket.on('disconnect',function(){
-					inp.style.backgroundColor='#FED';
-				});
-				
-				socket.on('joined',function(topic_){
-					if(topic==topic_){
-						inp.style.backgroundColor='#DFD';
-						inp.blur();
-					}
-				});
-				
-				
-				socket.on(topic,function(dat){
-					o.val=dat;
-				});
-				
-				if(!localSocket[topic]){
-					localSocket[topic]=[];
-				}
-				localSocket[topic].push(function(dat){
-					o.val=dat;
-				});
-			}
-			
-		}
-		
-		that.joinTopic();
-		
-		that.leaveTopic=function(){
-			if(that.vars.topic){
-				socket.emit('leaveTopic',that.vars.topic);
-			}
-		}
-		
-		inp.onchange=function(){
-			inp.style.backgroundColor='#FFF';
-			that.leaveTopic();
-			
-			if(inp.value){
-				that.vars.topic=inp.value;
-				that.joinTopic();
-			}
-		}
-	},
-	remove:function(i,o,that){
-		that.leaveTopic();
-		// we should fix localTopic too…
-	},
-	f:function(i,o,that){
-		if(that.vars.topic){
-			socket.emit(that.vars.topic,i.val);
-			var funcs=localSocket[that.vars.topic];
-			if(funcs){
-				for(f in funcs){
-					funcs[f](i.val); 
-				}
-			}
-		}
-	}
+    title:'Topic',
+    info:"Sends data to ANY other node with the same 'Topic Name'",
+    i:{val:0},
+    o:{val:0},
+    vars:{topic:""},
+    init:function(i,o,that){
+        var inp=document.createElement('input');
+        inp.type='input';
+        inp.value=that.vars.topic;
+        inp.placeholder='Topic Name';
+        that.widget.box.appendChild(inp);
+        that.widget.resize();
+        
+        that.joinTopic=function(){
+            
+            if(that.vars.topic){
+                
+                var topic=that.vars.topic;
+                
+                socket.emit('joinTopic',topic);
+                
+                socket.on('connect',function(){
+                    socket.emit('joinTopic',topic); // re-join the topic.
+                }); 
+                
+                socket.on('disconnect',function(){
+                    inp.style.backgroundColor='#FED';
+                });
+                
+                socket.on('joined',function(topic_){
+                    if(topic==topic_){
+                        inp.style.backgroundColor='#DFD';
+                        inp.blur();
+                    }
+                });
+                
+                
+                socket.on(topic,function(dat){
+                    o.val=dat;
+                });
+                
+                if(!localSocket[topic]){
+                    localSocket[topic]=[];
+                }
+                localSocket[topic].push(function(dat){
+                    o.val=dat;
+                });
+            }
+            
+        }
+        
+        that.joinTopic();
+        
+        that.leaveTopic=function(){
+            if(that.vars.topic){
+                socket.emit('leaveTopic',that.vars.topic);
+            }
+        }
+        
+        inp.onchange=function(){
+            inp.style.backgroundColor='#FFF';
+            that.leaveTopic();
+            
+            if(inp.value){
+                that.vars.topic=inp.value;
+                that.joinTopic();
+            }
+        }
+    },
+    remove:function(i,o,that){
+        that.leaveTopic();
+        // we should fix localTopic too…
+    },
+    f:function(i,o,that){
+        if(that.vars.topic){
+            socket.emit(that.vars.topic,i.val);
+            var funcs=localSocket[that.vars.topic];
+            if(funcs){
+                for(f in funcs){
+                    funcs[f](i.val); 
+                }
+            }
+        }
+    }
 }*/
 ];
-
-
 var nodeTypes = {};
 
 function makeTypes() {
@@ -654,7 +610,6 @@ function makeTypes() {
         nodeType(types[i]);
     }
 }
-
 //basically make a NodeType that inherits from Node
 function nodeType(dArg) {
     nodeTypes[dArg.type] = function NodeType(iArg) {
---
+++ js/graph/GraphNode.js
@@ -0,0 +1,14 @@
+/**
+ *  GraphNode 基础类
+ * @param {[type]} config [description]
+ */
+function GraphNode(config) {
+	this.inputs = {};
+	this.outputs = {};
+	this.slug = "名称变量名";
+	this.name = "用户定义的名称";
+
+	function initialConfigurations() {
+		
+	}
+}
